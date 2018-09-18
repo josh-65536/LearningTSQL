27cb5e7c8e6a07b3f806fb0c511a1f69df217c26
josh-65536 <40677877+josh-65536@users.noreply.github.com>
2018-09-18 01:50:36 -0500

    Add Section2: part 1

---
+++ Section2/01 Variables.sql
@@ -0,0 +1,17 @@
+
+DECLARE @foo int = 0;
+SELECT @foo;
+
+SET @foo = 3;
+SELECT @foo;
+
+
+DECLARE @tempTable TABLE (
+    FirstValue  int         NOT NULL PRIMARY KEY,
+    SecondValue varchar(20) NOT NULL
+);
+
+INSERT INTO @tempTable VALUES
+    (1, 'The'), (2, 'quick'), (3, 'brown'), (4, 'fox');
+
+SELECT * FROM @tempTable;
